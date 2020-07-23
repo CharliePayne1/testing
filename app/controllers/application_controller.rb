bc4914a616f679344d02ba579c379e1f636e986e
CharliePayne1 <charliepayne1101@gmail.com>
2020-07-23 16:49:20 +0100

    application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::API
+
+    def secret
+        ENV["SECRET"]
+    end
+
+    def generate_token(data)
+        JWT.encode(data, secret)
+    end
+
+    def decode_token
+        token = request.headers["Authorization"]
+        begin
+            JWT.decode(token, secret).first
+        rescue
+            {}
+        end
+    end
+    
+    def current_doctor
+        id = decode_token["id"]
+        Doctor.find_by(id: id)
+    end
+
 end
+
