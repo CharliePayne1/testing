5fb7bd9e6ebb752c61cb941b14f30608f8eb0e22
CharliePayne1 <charliepayne1101@gmail.com>
2020-07-23 16:51:24 +0100

    doctors controller added

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,2 +1,25 @@
 class DoctorsController < ApplicationController
+    
+    def login
+        doctor = Doctor.find_by(username: params[:username])
+        if doctor && doctor.authenticate(params[:password])
+            render json: { username: doctor.username, token: generate_token({ id: doctor.id})}
+        else
+            render json: { message: "Failure"}
+        end
+    end
+
+    def validate
+        doctor = current_doctor
+        render json: { username: doctor.username, token: generate_token({ id: doctor.id})}
+    end
+
+
+    private
+
+    def doctor_params
+        params.require(:doctor).permit(:username)
+    end
+
 end
+
