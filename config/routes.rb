68bd79778fa83fc38a7bfb753479fa5a3bdece01
CharliePayne1 <charliepayne1101@gmail.com>
2020-07-23 16:56:03 +0100

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :cases
-  resources :doctors
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  resources :cases, only: [:index, :show, :create, :update]
+
+  post '/login', to: 'doctors#login'
+  get '/validate', to: 'doctors#validate'
+
 end
+
