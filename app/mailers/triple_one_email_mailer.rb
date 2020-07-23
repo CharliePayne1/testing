0694aa92f6047f1c5e28bcb94be5732c3e02eee2
CharliePayne1 <charliepayne1101@gmail.com>
2020-07-23 16:57:46 +0100

    mailer added

---
+++ app/mailers/triple_one_email_mailer.rb
@@ -0,0 +1,9 @@
+class TripleOneEmailMailer < ApplicationMailer
+    default from: Rails.application.secrets.email_user_name
+
+    def decision_email(update_case)
+        @update_case = update_case
+        mail(to: "#{update_case.email}", subject: "A decision has been made about your case")
+    end
+    
+end
