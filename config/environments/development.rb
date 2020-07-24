1dbd53d179232d56c896d8a11198b36404dedec4
CharliePayne1 <charliepayne1101@gmail.com>
2020-07-24 13:13:58 +0100

    development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,8 +8,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :user_name            => Rails.application.secrets.email_user_name,
-  :password             => Rails.application.secrets.email_password,
+  :user_name            => ENV['email_user_name'],
+  :password             => ENV['email_password'],
   :authentication       => "plain",
   :enable_starttls_auto => true
   }
