f466bac9377a997bf2b6ba2663cd4850fa31ca6d
CharliePayne1 <charliepayne1101@gmail.com>
2020-07-27 14:18:54 +0100

    pessimistic rendering

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -16,14 +16,23 @@ class CasesController < ApplicationController
 
     def create
         new_case = Case.create(case_params)
+        if new_case.valid?
+            render json: new_case
+        else
+            render json: { error: "Could not create new case", message: new_case.errors.full_messages }
+        end
     end
 
     def update
         doctor = current_doctor
         update_case = Case.find_by(id: params[:id])
-        update_case.update(decision: params[:decision], doctor: doctor)
-        TripleOneEmailMailer.with(update_case: update_case).decision_email(update_case).deliver_now
-        render json: update_case
+        if update_case
+            update_case.update(decision: params[:decision], doctor: doctor)
+            TripleOneEmailMailer.with(update_case: update_case).decision_email(update_case).deliver_now
+            render json: update_case
+        else
+            render json: { error: "Could not update this case" }
+        end
     end
 
     private
