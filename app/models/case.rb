9a985b4b5e69968a88d8804323087181a02a6638
CharliePayne1 <charliepayne1101@gmail.com>
2020-07-27 12:04:29 +0100

    email validated

--- app/models/case.rb
+++ app/models/case.rb
@@ -2,6 +2,6 @@ class Case < ApplicationRecord
   belongs_to :doctor, optional: true
 
   validates :symptoms, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
 
 end
